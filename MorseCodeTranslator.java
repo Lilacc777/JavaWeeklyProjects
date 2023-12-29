677dd04c3e1ead2466ff80ed25db40f39307e4b7
Lilacc777 <lindsayli390@gmail.com>
2023-12-29 13:41:46 -0500

    Edited file for java project

--- MorseCodeTranslator.java
+++ MorseCodeTranslator.java
@@ -8,22 +8,34 @@ public class MorseCodeTranslator {
                 "-.-",  ".-..", "--",   "-.",   "---",
                 ".--.", "--.-", ".-.",  "...",  "-",
                 "..-",  "...-", ".--",  "-..-", "-.--",
-                "--..", "|" };
+                "--..", "|"};
         
-        String[] english = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", 
-                  "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-                  ",", ".", "?" };
+        char[] english = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+                'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                ',', '.', '?'};
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("To translate: ");
         String toTranslate = scanner.nextLine();
         toTranslate = toTranslate.toLowerCase();
-        char[] chars = toTranslate.toCharArray();
-
-        for(int i = 0; i < chars.length; i++){
-            
+        String[] list = toTranslate.split(" ");
+        
+        String answer = "";
+        for(int x = 0; x < list.length; x++){
+            for(int i = 0; i < list[x].length(); i++){
+                for(int j = 0; j < english.length; j++){
+                    if(list[x].charAt(i) == english[j]){
+                        answer = answer + morse[j] + " ";
+                    }
+                }
+            }
+            if(list[x] == list[list.length - 1]){
+                break;
+            }
+            answer += "/ ";
         }
+        System.out.println("Translated: " + answer);
         scanner.close();
     }
 }
